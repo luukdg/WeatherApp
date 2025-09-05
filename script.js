acb3f834720addb0a1b287510a04e4fc3e42e625
luukdg <luukdegraaf1993@gmail.com>
2025-09-05 22:16:03 +0200

    check weather with api, css styling

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-let location;
+let location = "Amsterdam";
+let ticketStatus = 0;
 
 const input = document.querySelector(".search");
 input.addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
     location = input.value;
     input.value = "";
+
+    document.querySelector(".weather").classList.add("weather-reveal");
+    document.querySelector(".weather-wrapper").classList.add("transition");
+    document.querySelector(".info-wrapper").classList.add("transition");
+
     getApi(location);
   }
 });
@@ -15,7 +21,6 @@ async function getApi(location) {
       `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&elements=datetime%2Cname%2Ctemp%2Cfeelslike%2Chumidity%2Cwindspeed%2Cconditions%2Cicon&include=current&key=RWJQU5E5U3KSXSXLDANJF79P6&contentType=json`
     );
     const weatherData = await response.json();
-    console.log(weatherData);
     const currentTempData = weatherData.currentConditions.temp;
     const conditionData = weatherData.currentConditions.conditions;
     const locationData = weatherData.address;
@@ -23,13 +28,22 @@ async function getApi(location) {
     const windSpeedData = weatherData.currentConditions.windspeed;
     const humidityData = weatherData.currentConditions.humidity;
 
-    console.log("Temperature:", currentTempData);
-    console.log("Condition:", conditionData);
-    console.log("Location:", locationData);
-    console.log("Feels Like:", feelsLikeData);
-    console.log("Wind Speed:", windSpeedData);
-    console.log("Humidity:", humidityData);
+    document.querySelector(".condition").textContent = conditionData;
+    document.querySelector(".location").textContent = locationData;
+    document.querySelector(".temp").textContent = currentTempData.toFixed(0);
+    document.querySelector(".feels-like").textContent =
+      "Feels like: " + feelsLikeData.toFixed(0) + " °C";
+    document.querySelector(".wind").textContent =
+      "Wind: " + windSpeedData + " km/h";
+    document.querySelector(".humidity").textContent =
+      "Humidity: " + humidityData + " %";
+
+    document.querySelector(".weather").classList.remove("weather-reveal");
+    document.querySelector(".weather-wrapper").classList.remove("transition");
+    document.querySelector(".info-wrapper").classList.remove("transition");
   } catch (err) {
     console.log("Location not found!");
   }
 }
+
+getApi(location);
